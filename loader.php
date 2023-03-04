dcab285953a97896feeabdf6f3578eb7dbd6058c
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2023-03-04 11:41:10 +0330

    Initial Commit

---
+++ loader.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copyright (c) Devinow (https://devinow.xyz/)
+ */
+
+$dir = __DIR__.'/src/';
+
+require_once $dir.'AbstractUpload.php';
+require_once $dir.'Base64Upload.php';
+require_once $dir.'DataUriUpload.php';
+require_once $dir.'Directory.php';
+require_once $dir.'File.php';
+require_once $dir.'FileUpload.php';
+
+?>
---
+++ src/Throwable/TotalSizeExceededError.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Copyright (c) Devinow (https://devinow.xyz/)
+ */
+
+namespace Devinow\FileUpload\Throwable;
+
+class TotalSizeExceededError extends Error {}
+
+?>
